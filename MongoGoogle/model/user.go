1c279ef95ba6ac74501187af575e4fb04bd761c8
Nemanja Ranitovic <nemanja.ranitovic@manufactoryteam.io>
2024-03-08 12:28:28 +0100

    Added user roles
    Users now have "user" status after registering to application.

---
+++ MongoGoogle/model/user.go
@@ -0,0 +1,54 @@
+package Model
+
+import "time"
+
+type GitHubData struct {
+	Username          string  `json:"login"`
+	Id                float64 `json:"id"`
+	NodeId            string  `json:"node_id"`
+	AvatarUrl         string  `json:"avatar_url"`
+	GravatarId        string  `json:"gravatar_id"`
+	Url               string  `json:"url"`
+	HtmlUrl           string  `json:"html_url"`
+	FollowersUrl      string  `json:"followers_url"`
+	FollowingUrl      string  `json:"following_url"`
+	GistsUrl          string  `json:"gists_url"`
+	StarredUrl        string  `json:"starred_url"`
+	SubscriptionsUrl  string  `json:"subscriptions_url"`
+	OrganizationsUrl  string  `json:"organizations_url"`
+	ReposUrl          string  `json:"repos_url"`
+	EventsUrl         string  `json:"events_url"`
+	RecievedEventsUrl string  `json:"recieved_events_url"`
+	Type              string  `json:"type"`
+	Name              string  `json:"name"`
+	Company           string  `json:"company"`
+	Blog              string  `json:"blog"`
+	Location          string  `json:"location"`
+	Email             string  `json:"email"`
+	Hireable          bool    `json:"hireable"`
+	Bio               string  `json:"bio"`
+	TwitterUsername   string  `json:"twitter_username"`
+	PublicRepos       int     `json:"public_repos"`
+	PublicGists       int     `json:"public_gists"`
+	Followers         int     `json:"followers"`
+	Role              string
+}
+type GoogleData struct {
+	RawData           map[string]interface{}
+	Provider          string
+	Email             string
+	Name              string
+	FirstName         string
+	LastName          string
+	NickName          string
+	Description       string
+	UserID            string
+	AvatarURL         string
+	Location          string
+	AccessToken       string
+	AccessTokenSecret string
+	RefreshToken      string
+	ExpiresAt         time.Time
+	IDToken           string
+	Role              string
+}
